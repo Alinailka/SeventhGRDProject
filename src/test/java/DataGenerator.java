bb77415addfcece51cf9451b447dcd350e09e026
Alinailka <alina-il@mail.ru>
2022-05-19 14:14:57 +0700

    Kill me.......

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -1,44 +1,100 @@
 import com.github.javafaker.Faker;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.experimental.UtilityClass;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-
-//@UtilityClass
-public class RegistrationByCardInfo {
-    Calendar c = new GregorianCalendar();
-    c.add(Calendar.DAY_OF_YEAR,5); // увеличиваем на 5 дня от текущей даты
-    SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-    String date1 = format1.format(c.getTime());
-
-    Calendar c2 = new GregorianCalendar();
-    c2.add(Calendar.DAY_OF_YEAR,7); // увеличиваем на 7 дня от текущей даты
-    SimpleDateFormat format2 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-    String date2 = format2.format(c2.getTime());
-    private static Faker faker;
-
-    String address = faker.address().cityName();
-    String name = faker.name().fullName();
-    String phone = faker.phoneNumber().phoneNumber();
+import java.util.Locale;
 
 
-}
-
-public class DataGenerator() {
+public class DataGenerator {
     private DataGenerator() {
     }
 
+    public String generateAddress(String local) {
+        Faker faker = new Faker(new Locale(local));
+       String address = faker.address().cityName();
+        return address;
+    }
+
+    public String generateDate1(int days1) {
+        Faker faker = new Faker();
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, days1); // увеличиваем на 5 дня от текущей даты
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+        String date1 = format1.format(c.getTime());
+        return date1;
+    }
+
+    public String generateName(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.name().fullName();
+    }
+
+    public String generatePhone(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public String generateDate2(int days2) {
+        Faker faker = new Faker();
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, days2); // увеличиваем на 5 дня от текущей даты
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+        String date2 = format1.format(c.getTime());
+        return date2;
+    }
+
     public static class Registration {
+
         private Registration() {
         }
 
-        public static RegistrationByCardInfo generateByCard(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationByCardInfo(faker.name().fullName(), faker.finance().
-                    creditCard(CreditCardType.MASTERCARD), LocalDate.now().plusYears(1));
+        public static UserInfo generateUser(String locale) {
+           // UserInfo user = new UserInfo(, );
+return new UserInfo();
         }
+//        public static RegistrationInfo generateUser(String local, int days1, int days2) {
+//
+//            RegistrationInfo user = new RegistrationInfo();
+//            Faker faker = new Faker(new Locale(local));
+//
+//            Calendar c = new GregorianCalendar();
+//            c.add(Calendar.DAY_OF_YEAR, days1); // увеличиваем на 5 дня от текущей даты
+//            SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+//            String date1 = format1.format(c.getTime());
+//
+//            Calendar c2 = new GregorianCalendar();
+//            c2.add(Calendar.DAY_OF_YEAR, days2); // увеличиваем на 7 дня от текущей даты
+//            SimpleDateFormat format2 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+//            String date2 = format2.format(c2.getTime());
+//
+//            String address = faker.address().cityName();
+//            String name = faker.name().fullName();
+//            String phone = faker.phoneNumber().phoneNumber();
+//            return user;}
+
+
+        @Value
+        public static class UserInfo {
+            String address;
+            String date1;
+            String name;
+            String phone;
+            String date2;
+//            return new RegistrationInfo(
+//                    faker.address().cityName(),
+//                    format1.format(c.getTime()),
+//                    faker.name().fullName(),
+//                    faker.phoneNumber().phoneNumber(),
+//                    format2.format(c2.getTime()));
+        }
+
     }
 }
+
+
--- src/test/java/RegistrationInfo.java
+++ src/test/java/RegistrationInfo.java
@@ -1,17 +1,12 @@
 import lombok.*;
 
 @Data
-@RequiredArgsConstructor
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 
 public class RegistrationInfo {
     private final String address;
+    private final String date1;
     private final String name;
     private final String phone;
-    private final String date1;
     private final String date2;
 }
 
